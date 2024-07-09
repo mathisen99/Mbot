e9f145aec6f3d0c32c54e83e221159f585403310
Tommy Mathisen <tommy.mathizen@gmail.com>
2024-07-09 22:46:02 +0300

    added clear messages if configs are missing and moved functions to utils

--- bot/utils.go
+++ bot/utils.go
@@ -1,10 +1,18 @@
 package bot
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
 	"regexp"
 	"strings"
+	"time"
+
+	"mbot/config"
 
 	"github.com/ergochat/irc-go/ircevent"
+	"github.com/fatih/color"
+	"github.com/joho/godotenv"
 )
 
 // ExtractNickname extracts the nickname from the full sender string
@@ -41,7 +49,7 @@ func FindURLs(message string) []string {
 	return re.FindAllString(message, -1)
 }
 
-// splitMessage splits a message into chunks based on the max length
+// SplitMessage splits a message into chunks based on the max length
 func SplitMessage(message string, maxLength int) []string {
 	var chunks []string
 
@@ -56,4 +64,65 @@ func SplitMessage(message string, maxLength int) []string {
 
 	chunks = append(chunks, message)
 	return chunks
+}
+
+// LoadEnv loads environment variables from the .env file
+func LoadEnv() {
+	if err := godotenv.Load(); err != nil {
+		color.Red("======================================================== NOTE ========================================================")
+		color.Red("Error loading .env file\nYou need to create a .env file in the root directory of the project or export the environment variables manually.\nIf you dont do this certain features will not work. They are optional but recommended.")
+		color.Red("======================================================================================================================")
+
+		sleepTime := 10
+		color.Yellow("Bot will Start in %d seconds", sleepTime)
+		time.Sleep(time.Duration(sleepTime) * time.Second)
+	}
+}
+
+// LoadConfig loads the bot configuration from the specified file path
+func LoadConfig(configPath string) (*config.Config, error) {
+	cfg, err := config.LoadConfig(configPath)
+	if err != nil {
+		color.Red("======================================================== NOTE ========================================================")
+		color.Red("Error loading config.json file\nYou need to create a config.json file in the data directory of the project.\nIf you dont do this the bot will not work.\nThere is an example file in the data directory named config_example.json.")
+		color.Red("Shutting down bot....")
+		color.Red("======================================================================================================================")
+		return nil, err
+	}
+	return cfg, nil
+}
+
+// LoadUsers loads the users from the specified file path
+func LoadUsersAtStart(filePath string) (map[string]User, error) {
+	users, err := LoadUsersFromFile(filePath)
+	if err != nil {
+		color.Red("======================================================== NOTE ========================================================")
+		color.Red("Error loading users.json file\nYou need to create a users.json file in the data directory of the project.\nIf you dont do this the bot will not work.\nThere is an example file in the data directory named users_example.json.")
+		color.Red("Shutting down bot....")
+		color.Red("======================================================================================================================")
+		return nil, err
+	}
+	return users, nil
+}
+
+// LoadUsersFromFile reads the users from a JSON file
+func LoadUsersFromFile(filePath string) (map[string]User, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("error opening users file: %w", err)
+	}
+	defer file.Close()
+
+	var users []User
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&users); err != nil {
+		return nil, fmt.Errorf("error decoding users file: %w", err)
+	}
+
+	userMap := make(map[string]User)
+	for _, user := range users {
+		userMap[user.Hostmask] = user
+	}
+
+	return userMap, nil
 }
--- main.go
+++ main.go
@@ -4,39 +4,22 @@ import (
 	"log"
 	"mbot/bot"
 	_ "mbot/commands"
-	"mbot/config"
 	"os"
-	"time"
-
-	"github.com/fatih/color"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	configPath := "./data/config.json"
-
 	// Load environment variables
-	if err := godotenv.Load(); err != nil {
-		color.Red("======================================================== NOTE ========================================================")
-		color.Red("Error loading .env file\nYou need to create a .env file in the root directory of the project or export the environment variables manually.\nIf you dont do this sertant features will not work. They are optional but recommended.")
-		color.Red("======================================================================================================================")
-
-		sleepTime := 10
-		color.Yellow("Bot will Start in %d seconds", sleepTime)
-		time.Sleep(time.Duration(sleepTime) * time.Second)
-	}
+	bot.LoadEnv()
 
 	// Load configuration
-	cfg, err := config.LoadConfig(configPath)
+	cfg, err := bot.LoadConfig("./data/config.json")
 	if err != nil {
-		color.Red("Error loading configuration, Did you forget to create a config.json file?")
 		os.Exit(1)
 	}
 
 	// Load users
-	bot.Users, err = bot.LoadUsers()
+	bot.Users, err = bot.LoadUsersAtStart("./data/users.json")
 	if err != nil {
-		color.Red("Error loading users: %v", err)
 		os.Exit(1)
 	}
 
